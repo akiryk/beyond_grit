3bf4366ca7f60e2c8bcf40bfab103b8891e0db6b
Adam Kiryk <adamkiryk@gmail.com>
2015-11-30 09:57:03 -0500

    Removing htpasswd lines.

--- front-page.php
+++ front-page.php
@@ -157,10 +157,4 @@ get_header(); ?>
     </main><!-- #main -->
   </div><!-- #primary -->
 
-  <?php
-$dir = dirname(__FILE__);
-echo "<p>Full path to this dir: " . $dir . "</p>";
-echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htpasswd" . "</p>";
-?>
-
 <?php get_footer(); ?>
