d7478a50a8db81599cfceb204aea9d576f344749
Adam Kiryk <adamkiryk@gmail.com>
2016-01-08 17:22:41 -0500

    Fix hero display issues on IE11

--- page.php
+++ page.php
@@ -13,22 +13,27 @@
  */
 
 get_header(); ?>
-	
-	<?php while ( have_posts() ) : the_post(); ?>
-		
-			
-		<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
+	<div id="primary" class="content-area">
+		<main id="main" class="content-main" role="main">
 
-		<?php
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-		?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				
+					<div class="inner-content">
+						<?php get_template_part( 'template-parts/content', 'page' ); ?>
+					</div>
 
-	<?php endwhile; // End of the loop. ?>
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
 
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
