ff046106742bc78f109139f05df8d73fcb6510f2
Adam Kiryk <adamkiryk@gmail.com>
2016-02-11 20:32:11 -0500

    Remove resource links; change tablet width for homepage; change frontpage images and sizes.

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
-  <?php
+   <?php
 /**
  * Custom home page based on underscore's page template.
  *
@@ -20,14 +20,11 @@ get_header(); ?>
             while( $query->have_posts() ){
               $query->the_post();
               echo '<h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
-              echo '<div class="entry-content  entry-content--fp">';
+              echo '<div class="entry-content--fp">';
               the_content();
               echo '</div>';
             }
           }
-
-          /* Display list of resource links if they exist. See inc/helpers.php */
-          print_menu_if_exists('Critical Thinking Resources', 'Resources');
 
           wp_reset_postdata();
         ?>
@@ -42,15 +39,11 @@ get_header(); ?>
           if ( $query->have_posts() ){
             while( $query->have_posts() ){
               $query->the_post();
-              echo '<h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
-              echo '<div class="entry-content entry-content--fp">';
+              echo '<div class="entry-content--fp-right"><h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
               the_content();
               echo '</div>';
             }
           }
-
-          /* Display list of resource links if they exist. See inc/helpers.php */
-          print_menu_if_exists('Community Leadership Resources', 'Resources');
 
           wp_reset_postdata();
         ?>
@@ -68,43 +61,14 @@ get_header(); ?>
             while( $query->have_posts() ){
               $query->the_post();
               echo '<h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
-              echo '<div class="entry-content entry-content--fp">';
+              echo '<div class="entry-content--fp">';
               the_content();
               echo '</div>';
             }
           }
 
-          /* Display list of resource links if they exist. See inc/helpers.php */
-          print_menu_if_exists('Social And Emotional Resources', 'Resources');
-
           /* Restore original post data */
           wp_reset_postdata();
-
-          /* Get the children of the Services page */
-          /*
-          $args = array(
-              'post_type' => 'page',
-              'post_parent' => $services_id
-          );
-
-          $services_query = new WP_query( $args );
-
-          if ( $services_query->have_posts() ){
-            echo '<ul class="services-list">';
-            while( $services_query->have_posts() ){
-              $services_query->the_post();
-              echo '<li class="clear">';
-                echo '<a href="' . get_permalink() . '" title="Learn more about ' . get_the_title() .'">';
-                echo '<h3 class="services-title">' . get_the_title() . '</h3>';
-                echo '</a>';
-                echo '<div class="services-lede">';
-                  the_content('My custom read more...');
-                echo '</div>';
-              echo '</li>';
-            }
-            echo '</ul>';
-          }
-          */
 
         ?>
         </div>
