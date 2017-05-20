a10974ea3a45393174548fc992467c6744067e7f
Adam Kiryk <adamkiryk@gmail.com>
2017-05-20 07:09:54 -0400

    Some style updates

--- single.php
+++ single.php
@@ -10,7 +10,6 @@
 get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="content-main" role="main">
-
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
@@ -19,9 +18,9 @@ get_header(); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+				// if ( comments_open() || get_comments_number() ) :
+				// 	comments_template();
+				// endif;
 			?>
 
 		<?php endwhile; // End of the loop. ?>
