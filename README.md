b7c363373851c73d39b95f2f9056c8c2372a70bb
Adam Kiryk <adamkiryk@gmail.com>
2017-09-01 12:56:29 -0400

    Add menu settings snapshot and update menu css

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 [![Build Status](https://travis-ci.org/Automattic/_s.svg?branch=master)](https://travis-ci.org/Automattic/_s)
 
-Beyond Grit
-===
-Theme for wordpress
+# Beyond Grit
 
-Site Structure
---------------
+## A theme for wordpress
 
-This site is a page-based site with static homepage and a blog. 
+### How to install
+
+* Start with a working version of Wordpress using MAMP locally, some other technique, or on a remote server
+* Place the theme into wp-content/themes/ directory
+* Enable the theme using Wordpress admin tools.
+
+### Update on the research2action website
+
+* Log in and go to `/research2action/wp-content/themes/beyond_grit`
+* Copy over any new files
+* Install/update any necessary plugins
+
+### Site Structure
+
+This site is a page-based site with static homepage and a blog.
 
 Enabling the blog along with the static homepage requires:
 * Create a file called front-page.php. This is a static page with custom content that pulls in parts of several pages.
@@ -21,7 +32,7 @@ Note: Optionally add a home.php file if you want to customize the display of the
 Navigation
 ----------
 
-The navigation uses one menu: 'Primary Menu'. This is a change from earlier versions of the site, which used a front-page-menu as well as the primary version. The navigation is rendered in `header.php` Note that there is conditional logic to wrap the navigation in different classes depending on whether it is on the home page or not. 
+The navigation uses one menu: 'Primary Menu'. This is a change from earlier versions of the site, which used a front-page-menu as well as the primary version. The navigation is rendered in `header.php` Note that there is conditional logic to wrap the navigation in different classes depending on whether it is on the home page or not.
 
 Fonts
 -----
--- front-page.php
+++ front-page.php
@@ -12,106 +12,106 @@
  */
 
 get_header(); ?>
-    <main id="main" class="content-main content-main--frontpage" role="main">
-      <section id="itzel-story" class="section section--fp">
-          <?php
-          // the loop
-          $query = new WP_query( 'pagename=itzel-story' );
-          if ( $query->have_posts() ){
-            while( $query->have_posts() ){
-              $query->the_post();
-              echo '<div class="inner-section inner-section--fp">';
-              edit_post_link(
-        sprintf(
-          /* translators: %s: Name of current post */
-          esc_html__( 'Edit %s', 'beyond_grit' ),
-          the_title( '<span class="screen-reader-text">"', '"</span>', false )
-        ),
-        '<span class="edit-link">',
-        '</span>'
-      );
-              echo '<div class="entry-content--fp entry-content--fp-centered">';
-              echo '<header class="story-heading">';
-              $subhead = get_post_meta(get_the_ID(), 'sub-heading', true);
-              echo '<h3 class="slug"><a href="' . get_permalink() . '">' . $subhead . '</a></h3>';
-              echo '<h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
-              echo '</header>';
-              the_content();
-              echo '</div>';
-              echo '</div>';
-
-              get_template_part( 'template-parts/content', 'get-report' );
-
-            }
-          }
-
-          wp_reset_postdata();
-        ?>
-      </section>
-
-      <section id="critical-thinking" class="section section--fp">
-        <div class="inner-section inner-section--fp">
+  <main id="main" class="content-main content-main--frontpage" role="main">
+    <section id="itzel-story" class="section section--fp">
         <?php
-          // the loop
-          $query = new WP_query( 'pagename=critical-thinking' );
-          if ( $query->have_posts() ){
-            while( $query->have_posts() ){
-              $query->the_post();
-              echo '<h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
-              echo '<div class="entry-content--fp">';
-              the_content();
-              echo '</div>';
-            }
+        // the loop
+        $query = new WP_query( 'pagename=itzel-story' );
+        if ( $query->have_posts() ){
+          while( $query->have_posts() ){
+            $query->the_post();
+            echo '<div class="inner-section inner-section--fp">';
+            edit_post_link(
+      sprintf(
+        /* translators: %s: Name of current post */
+        esc_html__( 'Edit %s', 'beyond_grit' ),
+        the_title( '<span class="screen-reader-text">"', '"</span>', false )
+      ),
+      '<span class="edit-link">',
+      '</span>'
+    );
+            echo '<div class="entry-content--fp entry-content--fp-centered">';
+            echo '<header class="story-heading">';
+            $subhead = get_post_meta(get_the_ID(), 'sub-heading', true);
+            echo '<h3 class="slug"><a href="' . get_permalink() . '">' . $subhead . '</a></h3>';
+            echo '<h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
+            echo '</header>';
+            the_content();
+            echo '</div>';
+            echo '</div>';
+
+            get_template_part( 'template-parts/content', 'get-report' );
+
           }
+        }
 
-          wp_reset_postdata();
-        ?>
-        </div>
-      </section>
+        wp_reset_postdata();
+      ?>
+    </section>
 
-      <section id="community" class="section section--fp">
-        <div class="inner-section inner-section--fp">
-        <?php
-          // the loop
-          $query = new WP_query( 'pagename=community-leadership-and-action' );
-          if ( $query->have_posts() ){
-            while( $query->have_posts() ){
-              $query->the_post();
-              echo '<div class="entry-content--fp-right"><h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
-              the_content();
-              echo '</div>';
-            }
+    <section id="critical-thinking" class="section section--fp">
+      <div class="inner-section inner-section--fp">
+      <?php
+        // the loop
+        $query = new WP_query( 'pagename=critical-thinking' );
+        if ( $query->have_posts() ){
+          while( $query->have_posts() ){
+            $query->the_post();
+            echo '<h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
+            echo '<div class="entry-content--fp">';
+            the_content();
+            echo '</div>';
           }
+        }
 
-          wp_reset_postdata();
-        ?>
+        wp_reset_postdata();
+      ?>
+      </div>
+    </section>
 
-        </div>
-      </section>
-      <section id="social-emotional" class="section section--fp">
-        <div class="inner-section inner-section--fp">
-        <?php
-          // the loop
-          $query = new WP_query( 'pagename=social-and-emotional-development' );
-          $services_id = $query->queried_object->ID;
-
-          if ( $query->have_posts() ){
-            while( $query->have_posts() ){
-              $query->the_post();
-              echo '<h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
-              echo '<div class="entry-content--fp">';
-              the_content();
-              echo '</div>';
-            }
+    <section id="community" class="section section--fp">
+      <div class="inner-section inner-section--fp">
+      <?php
+        // the loop
+        $query = new WP_query( 'pagename=community-leadership-and-action' );
+        if ( $query->have_posts() ){
+          while( $query->have_posts() ){
+            $query->the_post();
+            echo '<div class="entry-content--fp-right"><h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
+            the_content();
+            echo '</div>';
           }
+        }
 
-          /* Restore original post data */
-          wp_reset_postdata();
+        wp_reset_postdata();
+      ?>
 
-        ?>
-        </div>
-      </section>
+      </div>
+    </section>
+    <section id="social-emotional" class="section section--fp">
+      <div class="inner-section inner-section--fp">
+      <?php
+        // the loop
+        $query = new WP_query( 'pagename=social-and-emotional-development' );
+        $services_id = $query->queried_object->ID;
 
-    </main><!-- #main -->
+        if ( $query->have_posts() ){
+          while( $query->have_posts() ){
+            $query->the_post();
+            echo '<h2 class="section-title section-title--fp"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
+            echo '<div class="entry-content--fp">';
+            the_content();
+            echo '</div>';
+          }
+        }
+
+        /* Restore original post data */
+        wp_reset_postdata();
+
+      ?>
+      </div>
+    </section>
+
+  </main><!-- #main -->
 
 <?php get_footer(); ?>
